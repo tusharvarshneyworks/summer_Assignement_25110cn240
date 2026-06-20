7eed286807ef73af18dbf57c849fdc8aa89ad17c
tusharvarshneyworks <varshneytushar62585@gmail.com>
2026-06-20 15:15:34 +0530

    day-19 uploaded

---
+++ day19/Question-74.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main()
+{
+    int r,c;
+    printf("Enter rows and columns: ");
+    scanf("%d %d",&r,&c);
+
+    int a[r][c],b[r][c],sub[r][c];
+
+    printf("Enter first matrix:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("Enter second matrix:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            sub[i][j]=a[i][j]-b[i][j];
+        }
+    }
+
+    printf("Subtraction matrix:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",sub[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ day19/Question-76.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,sum=0;
+
+    printf("Enter the order of square matrix: ");
+    scanf("%d",&n);
+
+    int a[n][n];
+
+    printf("Enter matrix elements:\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        sum=sum+a[i][i];
+    }
+
+    printf("Diagonal sum = %d",sum);
+
+    return 0;
+}
